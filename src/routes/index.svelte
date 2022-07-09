ca2a57ba75659f0d3b846b4c2534f2f1a5d26486
Josh White <jojawhi@gmail.com>
2022-07-08 17:52:57 -0700

    updated index with smaller hero section and larger blog preview section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,74 @@
 <script>
   let blogSnippets = [
-    { title: 'Some Title', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-    { title: 'Some Title', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-    { title: 'Some Title', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' }
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    },
+    {
+      title: 'Some Title',
+      path: '/',
+      text: `Travel time to the nearest starbase? Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'?`
+    }
   ];
 </script>
 
 <div class="home">
   <section class="hero">
-    <h1 class="hero-title">Established approaches with fresh perspectives</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deserunt molestias
-      blanditiis delectus provident necessitatibus cupiditate facere tenetur perferendis autem culpa
-      in illum quibusdam, soluta possimus. Aspernatur explicabo saepe minima.
-    </p>
+    <div class="hero-text-container">
+      <h1 class="hero-title">Established approaches with fresh perspectives</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur deserunt molestias
+        blanditiis delectus provident necessitatibus cupiditate facere tenetur perferendis autem
+        culpa in illum quibusdam, soluta possimus. Aspernatur explicabo saepe minima.
+      </p>
+    </div>
+    <div class="hero-cta-container">
+      <a class="hero-cta" href="/">Create with us</a>
+    </div>
   </section>
   <section class="blog-snippets-section">
-    <h1 class="title">Wisdom Bites</h1>
     <div class="blog-snippets-container">
       {#each blogSnippets as blogSnippet}
         <div class="blog-snippet">
           <h3>{blogSnippet.title}</h3>
           <p>{blogSnippet.text}</p>
+          <a href={blogSnippet.path}>Read more -></a>
         </div>
       {/each}
     </div>
@@ -43,10 +90,29 @@
   @use 'src/styles/colors' as colors;
 
   .hero {
-    padding: 2rem;
+    display: grid;
+    grid-template-columns: 70% 30%;
     max-width: 100%;
-    height: 20vh;
     background-color: colors.$highlight;
+  }
+
+  .hero-text-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  .hero-title {
+    margin: 0;
+  }
+
+  .hero-cta-container {
+    padding: 2rem;
   }
 
   .blog-snippets-section {
