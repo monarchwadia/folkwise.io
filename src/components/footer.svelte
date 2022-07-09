7636b5be209983ea9a3eb5e603d7e571f90df8e6
Josh White <jojawhi@gmail.com>
2022-07-08 17:31:07 -0700

    updated footer.svelte

---
+++ src/components/footer.svelte
@@ -0,0 +1,47 @@
+<footer>
+  <div class="company-container">
+    <h3>Company</h3>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/blog">Blog</a></li>
+    </ul>
+  </div>
+  <div class="education-container">
+    <h3>Education</h3>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/blog">Blog</a></li>
+    </ul>
+  </div>
+</footer>
+
+<style type="scss">
+  @use 'src/styles/colors' as colors;
+
+  footer {
+    display: flex;
+    gap: 2rem;
+    padding: 2rem;
+    background-color: colors.$dark;
+
+    .company-container,
+    .education-container {
+      ul {
+        display: flex;
+        flex-direction: column;
+        padding: 0;
+      }
+    }
+
+    h3 {
+      color: colors.$light;
+    }
+
+    a {
+      text-decoration: none;
+      list-style: none;
+    }
+  }
+</style>
