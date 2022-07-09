1eb9d6e9a2fe39ba1d66a2d38d11ad2f6d3f9145
Josh White <jojawhi@gmail.com>
2022-07-08 17:28:53 -0700

    updated __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import Navbar from 'src/components/navbar.svelte';
+  import Footer from 'src/components/footer.svelte';
   import GlobalStyles from 'src/components/global-styles.svelte';
   import PostMetatags from 'src/components/post.metatags.svelte';
   import GoogleAnalytics from 'src/components/google-analytics.svelte';
@@ -13,6 +14,7 @@
     <div class="content-wrapper">
       <slot />
     </div>
+    <Footer />
   </div>
 </GlobalStyles>
 <PostMetatags />
@@ -21,18 +23,24 @@
 <style lang="scss">
   @use 'src/styles/colors' as colors;
 
+  .wrapper {
+    flex: 1 1 auto;
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    background-color: colors.$light;
+  }
+
   .navbar-wrapper {
     padding: 2vh 2vw;
     background-color: colors.$dark;
   }
 
   .content-wrapper {
-    padding: 2vh 2vw;
-    max-width: 600px;
+    flex: 1 1 auto;
+    min-height: 100%;
+    padding: 0;
+    max-width: 100%;
     margin: auto;
-  }
-
-  .wrapper {
-    background-color: colors.$light;
   }
 </style>
