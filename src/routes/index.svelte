797854990d79202ddc895c71b7d79629691cca20
Josh White <jojawhi@gmail.com>
2022-07-15 12:41:48 -0700

    refactored hero section to its own component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,6 +22,8 @@
 </script>
 
 <script lang="ts">
+  import Hero from '../components/hero.svelte';
+
   let blogSnippets = [
     {
       title: 'Some Title',
@@ -38,19 +40,7 @@
 </script>
 
 <div class="home">
-  <section class="hero">
-    <div class="hero-container">
-      <div class="hero-text-container">
-        <h1 class="large-header">We're a community of software developers.</h1>
-        <p class="bold-text">
-          We're custom software developers with deep expertise in a variety of industries.
-        </p>
-      </div>
-      <div class="hero-cta-container">
-        <a class="hero-cta" href="/contact">Let's grow.</a>
-      </div>
-    </div>
-  </section>
+  <Hero />
   <section class="blog-snippets-section">
     <div class="blog-snippets-container">
       {#each posts as post}
@@ -79,59 +69,6 @@
 
 <style type="scss">
   @use 'src/styles/colors' as colors;
-
-  .hero {
-    width: 100%;
-    background-color: colors.$medium;
-    background-image: url('/kikko-light.svg'),
-      linear-gradient(to left, colors.$white, colors.$white, colors.$light);
-    background-size: 90%, 100%;
-    background-position: -50% 50%, 100% 100%;
-    background-repeat: no-repeat;
-  }
-
-  .hero-container {
-    display: grid;
-    grid-template-columns: 60% 40%;
-    max-width: 1000px;
-    margin: 0 auto;
-  }
-
-  .hero-text-container {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    padding: 2rem 0;
-    background-color: rbga(0 0 0 0.5);
-    p {
-      margin: 0;
-    }
-  }
-
-  .hero-cta-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .hero-cta {
-    padding: 1rem 1.5rem;
-    color: colors.$highlight;
-    background-color: colors.$dark;
-    font-weight: 700;
-    text-decoration: none;
-    border-radius: 6px;
-    transition: all 300ms;
-
-    &:hover {
-      color: colors.$dark;
-      background-color: colors.$highlight;
-    }
-  }
-
-  .hero-cta-container {
-    padding: 2rem;
-  }
 
   .blog-snippets-section {
     margin: 0 auto;
