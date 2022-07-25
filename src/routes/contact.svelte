7add4a59b2b31425073bb81606862dba0a6b05bb
Josh White <jojawhi@gmail.com>
2022-07-25 14:58:47 -0700

    contact page styling

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,14 +1,22 @@
 <section class="contact-section">
   <div class="contact-container">
-    <h1>Get in touch with us</h1>
-    <div class="contact-form-container">
-      <label for="name">Name</label>
-      <input type="text" class="text" id="name" />
-      <label for="email">Email</label>
-      <input type="text" class="text" id="email" />
-      <label for="message">Message</label>
-      <textarea name="message" id="message" cols="30" rows="10" />
-    </div>
+    <form class="contact-form-container">
+      <h1>Get in touch with us</h1>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="text" id="name" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" class="text" id="email" required />
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10" required />
+      </div>
+
+      <button type="submit">Send</button>
+    </form>
   </div>
 </section>
 
@@ -21,34 +29,57 @@
     justify-content: center;
     width: 100%;
     max-width: 750px;
-    padding: 1rem;
     margin: 1rem auto;
     background-color: colors.$white;
-
-    h1 {
-      margin-left: 1rem;
-    }
   }
 
   .contact-form-container {
     display: flex;
     flex-direction: column;
     align-self: center;
+    gap: 1rem;
     width: 100%;
-    max-width: 500px;
-    padding: 1rem;
+    max-width: 700px;
+    padding: 2rem 1rem;
 
-    label {
-      margin-top: 1rem;
+    button {
+      padding: 0.75rem 1.5rem;
+      color: colors.$highlight;
+      background-color: colors.$dark;
+      font-weight: 700;
+      text-decoration: none;
+      border-radius: 4px;
+      border: none;
+      outline: none;
+      transition: all 300ms;
+
+      &:hover {
+        color: colors.$dark;
+        background-color: colors.$highlight;
+      }
     }
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
 
     input,
     textarea {
       color: colors.$white;
-      background-color: colors.$dark;
+      background-color: colors.$dark-50;
       outline: none;
       border: none;
       padding: 0.5rem;
+      transition: all 300ms;
+      margin: 0.25rem 0;
+      border-radius: 3px;
+
+      &:focus {
+        background-color: colors.$dark;
+        outline: 1px solid colors.$highlight-red;
+        box-shadow: 0 0 4px 1px colors.$highlight-red, inset 0 0 4px 1px colors.$highlight-red;
+      }
     }
   }
 </style>
