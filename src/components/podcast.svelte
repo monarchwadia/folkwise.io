2c54e2aa2f49a2c9a8c15611b45e5b42e6fd80f8
jojawhi <jojawhi@gmail.com>
2022-08-03 13:17:04 +0900

    refatored podcast component to use spotify 'embed show'; added listen on... links, added responsive behaviours to podcast component

--- src/components/podcast.svelte
+++ src/components/podcast.svelte
@@ -1,43 +1,56 @@
 <script lang="ts">
-  let source: string;
+  let podcastURLs = [
+    {
+      liveURL: 'https://podcasts.apple.com/us/podcast/the-folkwise-podcast/id1636400853',
+      imgURL: '/assets/apple-podcasts-badge.svg'
+    },
+    {
+      liveURL:
+        'https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5yZWRjaXJjbGUuY29tLzZlNGU2NzFkLTFhMDEtNGNmYi05ODBmLWY5YWVjMGRkZWZkYg==',
+      imgURL: '/assets/google-podcasts-badge.svg'
+    },
+    {
+      liveURL: 'https://www.stitcher.com/show/the-folkwise-podcast',
+      imgURL: '/assets/stitcher-podcasts-badge.png'
+    },
+    {
+      liveURL: 'https://radiopublic.com/the-folkwise-podcast-6rXjAa',
+      imgURL: '/assets/radiopublic-podcasts-badge.png'
+    }
+  ];
 </script>
 
 <div class="podcast-container">
+  <!-- Spotify embed -->
+
   <div class="player-container">
-    <img
-      class="podcast-thumb"
-      src="/assets/Folkwise_podcast_cover.png"
-      alt="people around a gold sphere"
-      srcset=""
+    <iframe
+      style="border-radius:12px; box-shadow: 0 0 4px 0 white"
+      src="https://open.spotify.com/embed/show/73RAoxiaE3Qs08EvePd8io?utm_source=generator&theme=0"
+      width="100%"
+      height="232"
+      title="folkwise podcast"
+      frameBorder="0"
+      allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
     />
-    <audio controls>
-      <source src="https://stream.redcircle.com/episodes/4b9dab95-8e3f-4a46-8335-37609e3584f9/stream.mp3" />
-    </audio>
   </div>
   <div class="info-container">
-    <div class="episode-info">
-      <h2>Episode 1</h2>
-      <p>
-        Meet the host, and meet the team! David moderates a great discussion where Monarch breaks
-        down what he thinks is missing in the tech industry and the gang gets sidetracked
-        reminiscing about old video games.
-      </p>
+    <div class="host">
+      <p>Hosted by</p>
+      <img src="/assets/david-optimized.jpg" alt="host david" />
+      <p class="bold-text">David Marshall</p>
     </div>
-    <div class="guests-container">
-      <div class="guest">
-        <p class="bold-text">Host</p>
-        <img src="/assets/david-optimized.jpg" alt="host david" />
-        <p>David Marshall</p>
-      </div>
-      <div class="guest">
-        <p class="bold-text">Guest</p>
-        <img src="/assets/monarch-optimized.jpg" alt="host david" />
-        <p>Monarch Wadia</p>
-      </div>
-      <div class="guest">
-        <p class="bold-text">Guest</p>
-        <img src="/assets/josh.jpg" alt="host david" />
-        <p>Josh White</p>
+
+    <div class="listen-info">
+      <p>Listen wherever you get your podcasts:</p>
+      <div class="listen-links-container">
+        {#each podcastURLs as podcastURL}
+          <div class="listen-link">
+            <a href={podcastURL.liveURL} target="_blank" rel="noopener noreferrer">
+              <img src={podcastURL.imgURL} alt="podcast provider badge" srcset="" />
+            </a>
+          </div>
+        {/each}
       </div>
     </div>
   </div>
@@ -48,8 +61,9 @@
 
   .podcast-container {
     display: flex;
+    flex-direction: column;
     gap: 1rem;
-    padding: 3rem 2rem;
+    padding: 1rem 2rem;
     max-width: 750px;
     margin: 0 auto;
     border-radius: 25px;
@@ -72,29 +86,13 @@
   }
 
   .info-container {
-    display: flex;
-    flex-direction: column;
-    max-width: 60%;
-    justify-content: space-between;
-  }
-
-  .podcast-thumb {
-    width: 100%;
-    box-shadow: 0 0 6px 2px colors.$medium;
-  }
-
-  audio {
-    width: 100%;
-    margin-top: 1rem;
-  }
-
-  .guests-container {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 0.75rem;
+    grid-template-columns: 20% 65%;
+    gap: 1.5rem;
+    width: 100%;
   }
 
-  .guest {
+  .host {
     display: flex;
     flex-direction: column;
     gap: 0.25rem;
@@ -102,12 +100,72 @@
     text-align: center;
 
     img {
-      width: 70%;
+      width: 100%;
       border-radius: 50%;
     }
 
     p {
       font-size: 0.75rem;
+    }
+  }
+
+  .listen-info {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    p {
+      font-size: 0.75rem;
+    }
+  }
+
+  .listen-links-container {
+    display: grid;
+    grid-template-columns: 8.75rem 8.75rem;
+    grid-template-rows: 1fr 1fr;
+    align-items: center;
+  }
+
+  .listen-link {
+    img {
+      max-width: 8rem;
+    }
+  }
+
+  @media screen and (max-width: 520px) {
+    .podcast-container {
+      background-image: linear-gradient(
+          to bottom,
+          colors.$dark,
+          colors.$dark-50,
+          colors.$dark,
+          colors.$dark
+        ),
+        url('/assets/kikko-light-optimized.svg');
+    }
+
+    .info-container {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .host {
+      padding: 1rem 0;
+      border-top: solid 1px colors.$white;
+      border-bottom: solid 1px colors.$white;
+
+      img {
+        width: 45%;
+      }
+    }
+
+    .listen-info {
+      text-align: center;
+    }
+
+    .listen-links-container {
+      display: flex;
+      flex-direction: column;
     }
   }
 </style>
