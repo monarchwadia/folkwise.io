5fb73bc819240418ff2b8355f200b1b7fb81e2ff
Josh White <jojawhi@gmail.com>
2022-10-11 14:13:09 -0700

    reverted back to process.env.SECRET_VAR for serverConfig to work with Vercel

--- src/lib/server/serverConfig.ts
+++ src/lib/server/serverConfig.ts
@@ -1,6 +1,10 @@
-import { HCAPTCHA_SECRET } from '$env/static/private';
-import { SENDGRID_SECRET } from '$env/static/private';
-import { SENDGRID_SENDER_EMAIL } from '$env/static/private';
+// import { HCAPTCHA_SECRET } from '$env/static/private';
+// import { SENDGRID_SECRET } from '$env/static/private';
+// import { SENDGRID_SENDER_EMAIL } from '$env/static/private';
+
+// **** New import method for .env variables not currently supported by Vercel
+// **** These will work in dev mode, but for production, process.env will pull
+// **** the variables that you enter into Vercel's Environment Variable UI
 
 //import any secret env variables into this file and then assign them to the config object
 //then import serverConfig where you need to access them
@@ -13,9 +17,9 @@ if (browser) {
 }
 
 const serverConfig = {
-  sendgridSecret: SENDGRID_SECRET,
-  sendgridSenderEmail: SENDGRID_SENDER_EMAIL,
-  hCaptchaSecret: HCAPTCHA_SECRET
+  sendgridSecret: process.env.SENDGRID_SECRET,
+  sendgridSenderEmail: process.env.SENDGRID_SENDER_EMAIL,
+  hCaptchaSecret: process.env.HCAPTCHA_SECRET
 };
 
 export default serverConfig;
