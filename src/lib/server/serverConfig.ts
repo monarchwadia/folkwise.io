36e3e628c00a80f8f98c231043e6710820a2a789
Josh White <jojawhi@gmail.com>
2022-10-11 14:58:21 -0700

    added logic for determining production or dev env variable access method

--- src/lib/server/serverConfig.ts
+++ src/lib/server/serverConfig.ts
@@ -1,13 +1,10 @@
-// import { HCAPTCHA_SECRET } from '$env/static/private';
-// import { SENDGRID_SECRET } from '$env/static/private';
-// import { SENDGRID_SENDER_EMAIL } from '$env/static/private';
+import { HCAPTCHA_SECRET } from '$env/static/private';
+import { SENDGRID_SECRET } from '$env/static/private';
+import { SENDGRID_SENDER_EMAIL } from '$env/static/private';
 
 // **** New import method for .env variables not currently supported by Vercel
 // **** These will work in dev mode, but for production, process.env will pull
 // **** the variables that you enter into Vercel's Environment Variable UI
-
-//import any secret env variables into this file and then assign them to the config object
-//then import serverConfig where you need to access them
 
 // import 'dotenv/config';
 import { browser } from '$app/environment';
@@ -16,10 +13,24 @@ if (browser) {
   throw new Error('import of server config from browser');
 }
 
+const getEnvValue = (key: string) => {
+  let value;
+  try {
+    value = process.env[key];
+  } catch (e) {
+    try {
+      value = key;
+    } catch (e) {
+      return undefined;
+    }
+  }
+  return value;
+};
+
 const serverConfig = {
-  sendgridSecret: process.env.SENDGRID_SECRET,
-  sendgridSenderEmail: process.env.SENDGRID_SENDER_EMAIL,
-  hCaptchaSecret: process.env.HCAPTCHA_SECRET
+  sendgridSecret: getEnvValue(SENDGRID_SECRET),
+  sendgridSenderEmail: getEnvValue(SENDGRID_SENDER_EMAIL),
+  hCaptchaSecret: getEnvValue(HCAPTCHA_SECRET)
 };
 
 export default serverConfig;
